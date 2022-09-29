b77175baf8f77e328e4c916e002c4035bc7e454c
Oceanne Wang <oceanneywang@gmail.com>
2022-09-29 10:41:23 -0700

    Scatterplot generated for the vroom dataset; comments added

--- Biol4315_Lab2_Test.R
+++ Biol4315_Lab2_Test.R
@@ -1,3 +1,8 @@
+#Load mtcars dataset
 data(mtcars)
+#Assign to a variable
 vroom <- mtcars
+#Check the class of variable
 class(vroom)
+#Scatter plot of vroom
+plot(vroom$mpg,vroom$hp, main = "Scatterplot of mtcars", xlab = "mpg", ylab = "hp")
